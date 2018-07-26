1bd7df5ca10eaf95a22fc91c5329812848cd3c71
cailun <cailun@s-privacy.com>
2018-07-26 17:08:41 +0800

    Initial commit

---
+++ src/main/java/cn/sprivacy/yuantong/YuantongApplication.java
@@ -0,0 +1,19 @@
+package cn.sprivacy.yuantong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class YuantongApplication extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YuantongApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(this.getClass());
+    }
+}
