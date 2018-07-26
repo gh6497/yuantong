a33039f5fba54b09da6edc1f26013bf6a0fb230c
cailun <cailun@s-privacy.com>
2018-07-26 17:15:44 +0800

    2

---
+++ src/main/java/cn/sprivacy/yuantong/domain/AppFile.java
@@ -0,0 +1,26 @@
+package cn.sprivacy.yuantong.domain;
+
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+import java.util.Date;
+
+/**
+ * @author: cailun
+ * @date: 2018年 07月26日
+ */
+@Data
+@Accessors(chain = true)
+public class AppFile {
+    private Integer id;
+
+    private String fileName;
+
+    private Date submitTime;
+
+    private Byte uploadByFile;
+
+    private Integer result;
+
+    private Integer ownerId;
+}
---
+++ src/main/java/cn/sprivacy/yuantong/domain/AppVersion.java
@@ -0,0 +1,8 @@
+package cn.sprivacy.yuantong.domain;
+
+/**
+ * @author: cailun
+ * @date: 2018年 07月26日
+ */
+public class AppVersion {
+}
